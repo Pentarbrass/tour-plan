a8faef6e19a5cb4cf6211ab53442846e46a767ce
Pentarbrass <alexand.bybur@gmail.com>
2021-03-08 18:36:52 +0200

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+    },
+    
+});
+var swiper = new Swiper('.swiper-container', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
