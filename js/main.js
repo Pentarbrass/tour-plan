4e4768ac6fc9c5836c855c8b082bb0b60d76d510
Pentarbrass <alexand.bybur@gmail.com>
2021-03-10 12:55:42 +0200

    Create: reviews section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-var swiper = new Swiper('.swiper-container', {
+var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
-    },
-    
-});
-var swiper = new Swiper('.swiper-container', {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
 });
+var reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
+
 // ymaps.ready(function () {
 //   var myMap = new ymaps.Map('map', {
 //     center: [7.890703, 98.294772],
